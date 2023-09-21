a41d406d6596f1fbac85ee2d096c25d8225dc887
Roberta <federicoroberta1@gmail.com>
2023-09-21 12:12:35 +0200

    add project Holiday

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-import { useState, useEffect } from "react";
-import data from "./data";
-import Articolo from "./Articolo";
-
-//Funzione che se presente 'Theme' nel localStorage
-const getFromLocalStorage = () => {
-  if (localStorage.getItem("theme")) {
-    return localStorage.getItem("theme");
-  } else {
-    return "light-mode";
-  }
-};
-// returna il suo valore o di default return 'light-mode'
-
+import Title from "./components/Title";
+import Holiday from "./components/Holiday";
+//Ho utilizzato Cartella Component per dare una chiara locazione ai miei componenti
 function App() {
-  const [theme, setTheme] = useState(getFromLocalStorage());
-
-  const changeTheme = () => {
-    if (theme === "light-mode") {
-      setTheme("dark-mode");
-    } else {
-      setTheme("light-mode");
-    }
-  };
-
-  useEffect(() => {
-    document.documentElement.className = theme;
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
   return (
     <section className="section-center">
       <div className="container">
-        <button className="btn" onClick={changeTheme}>
-          Cambia
-        </button>
-        <section className="article-section">
-          {data.map((el) => (
-            <Articolo key={el.id} {...el} />
-          ))}
-        </section>
+        <Title text="Le Nostre Vacanze" />
+        <Holiday />
       </div>
     </section>
   );
